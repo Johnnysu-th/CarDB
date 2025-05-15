2466e2137014d3ed3c4fc9572db3a2a94f37b704
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 12:05:28 +1200

    publish finished

--- tst.py
+++ tst.py
@@ -1,10 +1,124 @@
+#docstring- SteveRod
+
+
+
+
+
+
 import sqlite3
 
-db = sqlite3.connect("car.db")
-cursor = db.cursor()
-sql = "Select * from car;"
-cursor.execute(sql)
-results = cursor.fethall()
-print(results)
 
-db.close()
+
+
+DATABASE = "car.db"
+
+
+
+
+
+ 
+#functionss
+def print_all_name():
+    '''print all the name nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "Select * from car;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name           top_speed       country    maker    id")
+    for car in results:
+        #print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}{car[4]:<4}{car[5]:<4}{car[6]:<4}")
+        print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}")
+        #loop finished here
+    db.close()
+
+def print_all_name_by_topspeed():
+    '''print all the name by topspeed'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "Select * from carORDER BY top_speed DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name           top_speed       country    maker    id")
+    for car in results:
+        #print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}{car[4]:<4}{car[5]:<4}{car[6]:<4}")
+        print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}")
+        #loop finished here
+    db.close()
+
+def print_all_name_by_country():
+    '''print all the name storted by country'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "Select * from carORDER BY country DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name           top_speed       country    maker    id")
+    for car in results:
+        #print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}{car[4]:<4}{car[5]:<4}{car[6]:<4}")
+        print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}")
+        #loop finished here
+    db.close()
+
+def print_all_name_by_maker():
+    '''print all the name storted by maker'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "Select * from carORDER BY maker DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name           top_speed       country    maker    id")
+    for car in results:
+        #print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}{car[4]:<4}{car[5]:<4}{car[6]:<4}")
+        print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}")
+        #loop finished here
+    db.close()
+
+def print_all_name_by_id():
+    '''print all the name storted by id'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "Select * from carORDER BY id DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name           top_speed       country    maker    id")
+    for car in results:
+        #print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}{car[4]:<4}{car[5]:<4}{car[6]:<4}")
+        print(f"{car[1]:<30}{car[2]:<8}{car[3]:<4}")
+        #loop finished here
+    db.close()
+
+
+#main code
+while True:
+    user_input = input("""
+What would you like to do.
+1.Print all name
+2.Print all name sorted by topspeed
+3.Print all name sorted by country
+4.Print all name sorted by maker
+5.Print all name sorted by id
+6.Exit
+""")
+    if user_input == "1":
+        print_all_name()
+    elif user_input == "2":
+        print_all_name_by_topspeed()
+    elif user_input == "3":
+        print_all_name_by_country()
+    elif user_input == "4":
+        print_all_name_by_maker()
+    elif user_input == "5":
+        print_all_name_by_id()
+    elif user_input == "6":
+        break
+    
+    else:
+        print("That was not a option\n")
+
+
